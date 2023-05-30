95561d909a8ca69a8ad97726e6963a30af07a498
ranaom222 <ovrana2002@gmail.com>
2023-05-30 19:27:09 +0530

    Changes Made

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Searchbar from "./Components/Searchbar";
+import HomePg from "./Pages/HomePg";
+import LoginPg from "./Pages/LoginPg";
+import ProductPg from "./Pages/ProductPg";
+import RegPg from "./Pages/RegPg";
+import CartPg from "./Pages/CartPg";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Searchbar />
+        <Routes>
+          <Route path="/" Component={HomePg} />
+          <Route path="/login" Component={LoginPg} />
+          <Route path="/register" Component={RegPg} />
+          <Route path="/product-page" Component={ProductPg} />
+          <Route path="/cart-page" Component={CartPg} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
